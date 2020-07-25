0a23af140aa3a12dc6163db3499ea3d59cb4ffa5
Israel <israel.facanha@gmail.com>
2020-07-25 18:49:58 -0300

    Add reaturant model

---
+++ meat-api/src/restaurants/restaurants.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+export interface MenuItem extends mongoose.Document {
+  name: string;
+  price: number;
+}
+
+export interface RestaurantDocument extends mongoose.Document {
+  name: string;
+  menu: Array<MenuItem>;
+}
+
+const menuSchema = new mongoose.Schema<MenuItem>({
+  name: {
+    type: String,
+    required: [true, "Menu name is requeired!"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Menu price is required!"],
+  },
+});
+
+const restaurantSchema = new mongoose.Schema<RestaurantDocument>({
+  name: {
+    type: String,
+    required: [true, "Restaurant name is required!!"],
+  },
+  menu: {
+    type: [menuSchema],
+    select: false,
+    default: [],
+  },
+});
+
+export const Restaurant = mongoose.model<RestaurantDocument>(
+  "restaurant",
+  restaurantSchema
+);
